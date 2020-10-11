e699f1062d38d4ae1a5882007bed38558993298f
Migrig <konstantine.grigorovich@gmail.com>
2020-10-11 23:27:40 +0300

    Lab
    Lab

---
+++ C++/Lab-1/1.2.cpp
@@ -0,0 +1,74 @@
+﻿//Григорович Константин ИВТ-8, 1 вариант , 1 задание , 2 программа
+
+/*Преобразование: 1D → 2D. Одномерный массив из 25 вещественных чисел необходимо
+разложить по двумерной сетке 5х5 слева направо и сверху вниз, но первый элемент на
+каждой строке должен содержать сумму остальных четырех.
+Инициализация: заполнить массив числами x[n] = n ∙ sin(πn/25), n – индекс элемента.
+Вывод на экран: на каждый элемент массива отвести 10 позиций.*/
+
+#include<iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <math.h>
+#include <cstring>
+
+using namespace std; // чтобы не записывать std::cin
+
+int i;//Индекс элемента
+int rows = 5;//Строки
+int cols = 5;//Столбцы
+float* arr = new float[25];//Место под массив
+void arrayA(float* arr)
+{
+	for (i = 0; i < 25; i++)
+	{
+		arr[i] = i * sin((3.14 * i) / 25);
+		cout << setw(10) << arr[i];
+	}
+}
+float** array2d(float** arr2d, float* arr, size_t  rows, size_t  cols)
+{
+	int coun = 0;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			*(*(arr2d + i) + j) = *(arr + i * cols + j);
+			cout << setw(10) << arr2d[i][j];
+			//coun++;
+		}
+		coun++;
+		cout << endl;
+	}
+	return arr2d;
+}
+void summ(float** arr2d)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << setw(10);
+			arr2d[i][0] = arr2d[i][j + 1] + arr2d[i][j + 2] + arr2d[i][j + 3] + arr2d[i][j + 4];
+			cout << arr2d[i][j];
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");//Русский язык в консоли
+	float** arr2d = new float* [rows];//Выделяем место под 2Д
+	for (int i = 0; i < rows; i++)
+	{
+		arr2d[i] = new float[cols];
+	}
+	arrayA(arr);
+	cout << endl;
+	cout << "Массив создан" << endl;
+	array2d(arr2d, arr, rows, cols);
+	cout << endl;
+	cout << "Массив, с 1 строкой - суммой всех элементов строки" << endl;
+	summ(arr2d);
+	return 0;
+}
---
+++ C++/Lab-1/1.3.cpp
@@ -0,0 +1,55 @@
+﻿//Григорович Константин ИВТ-8, 1 вариант , 1 задание , 3 программа
+
+/*Функция strchr.
+Формат char* strchr(char* s, int c).
+Функция находит в строке s первое вхождение символа с и возвращает подстроку,
+начинающуюся с этого символа.*/
+
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+char* rest_of_string = 0; //Часть строки, которую необходимо вернуть
+char* SC(char* s, int c)
+{
+    size_t len = 0;//Начальный инедкс
+    while (s[len] != '\0')
+    {
+        if (s[len] == c) rest_of_string = s + len;//поиск нужного символа
+        len++;
+    }
+    return rest_of_string;
+}
+int main()
+{
+    char s[] = "Sabaton - camouflage";
+    int  ch = 'c';
+    char* pdest;
+    int result;
+    char* standart_result = strrchr(s, 'c');
+    setlocale(LC_ALL, "Russian");//Русский язык в консоли
+    printf_s("Строка для поиска:\n      %s\n", s);
+
+    //cout << strchr(s,5);
+
+    printf_s("Поиск по символу:   %c\n", ch);
+    printf("Строчка с буквы :%s\n", standart_result);
+    // Search forward.
+    pdest = strchr(s, ch);
+    result = (int)(pdest - s + 1);
+    if (pdest != NULL)
+        printf_s("Результат:   первая %c в строке  %d\n",
+            ch, result);
+    else
+        printf_s("Результат:  последняя %c не найдено\n", ch);
+
+    // Search backward.
+    pdest = strrchr(s, ch);
+    result = (int)(pdest - s + 1);
+    if (pdest != NULL)
+        printf_s("Результат:   последняя %c в строке %d\n", ch, result);
+    else
+        printf_s("Результат:\t%c не найдено\n", ch);  
+}
